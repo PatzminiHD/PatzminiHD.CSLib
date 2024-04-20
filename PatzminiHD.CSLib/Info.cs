1c7acf425b231bccbc2220b23c53daacb50f37dc
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2024-04-20 23:14:17 +0200

    Initial commit

---
+++ PatzminiHD.CSLib/Info.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace PatzminiHD.CSLib
+{
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
+    public sealed class Info : Attribute
+    {
+        public static string Name = "PatzminiHD.CSLib";
+        public static string Version = "v0.1.0";
+        public Info(string buildTimeStamp)
+        {
+            BuildTime = DateTime.ParseExact(buildTimeStamp, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None);
+        }
+
+        public DateTime BuildTime { get; set; }
+
+        public static DateTime BuildTimeStamp
+        {
+            get
+            {
+                var assembly = typeof(Info).Assembly;
+                var attribute = assembly.GetCustomAttribute<Info>();
+
+                return attribute != null ? attribute.BuildTime : DateTime.MinValue;
+            }
+        }
+    }
+
+    
+}
