9bc44476d3ad75937f72f287cc0b2ffaf34b9168
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2024-05-01 00:56:35 +0200

    Implement DropDown Selector; Fix #2

--- PatzminiHD.CSLib/Input/Console.cs
+++ PatzminiHD.CSLib/Input/Console.cs
@@ -157,13 +157,6 @@ namespace PatzminiHD.CSLib.Input
                 System.Console.Write('╝');
             }
 
-            private static void SwapForegroundBackgroundColor()
-            {
-                var tmp = System.Console.ForegroundColor;
-                System.Console.ForegroundColor = System.Console.BackgroundColor;
-                System.Console.BackgroundColor = tmp;
-            }
-
             private static void DrawResponses(int xLeft, int y, ResponseOptions responseOptions, Response selectedResponse)
             {
                 switch(responseOptions)
@@ -172,7 +165,7 @@ namespace PatzminiHD.CSLib.Input
                         switch(selectedResponse)
                         {
                             case Response.OK:
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 3, y);
                                 System.Console.Write("OK");
                                 System.Console.ResetColor();
@@ -189,7 +182,7 @@ namespace PatzminiHD.CSLib.Input
                             case Response.OK:
                                 System.Console.SetCursorPosition(xLeft - 7, y);
                                 System.Console.Write("Cancel");
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 10, y);
                                 System.Console.Write("OK");
                                 System.Console.ResetColor();
@@ -197,7 +190,7 @@ namespace PatzminiHD.CSLib.Input
                             case Response.CANCEL:
                                 System.Console.SetCursorPosition(xLeft - 10, y);
                                 System.Console.Write("OK");
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 7, y);
                                 System.Console.Write("Cancel");
                                 System.Console.ResetColor();
@@ -216,7 +209,7 @@ namespace PatzminiHD.CSLib.Input
                             case Response.NO:
                                 System.Console.SetCursorPosition(xLeft - 7, y);
                                 System.Console.Write("Yes");
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 3, y);
                                 System.Console.Write("No");
                                 System.Console.ResetColor();
@@ -224,7 +217,7 @@ namespace PatzminiHD.CSLib.Input
                             case Response.YES:
                                 System.Console.SetCursorPosition(xLeft - 3, y);
                                 System.Console.Write("No");
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 7, y);
                                 System.Console.Write("Yes");
                                 System.Console.ResetColor();
@@ -245,7 +238,7 @@ namespace PatzminiHD.CSLib.Input
                                 System.Console.Write("No");
                                 System.Console.SetCursorPosition(xLeft - 14, y);
                                 System.Console.Write("Yes");
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 7, y);
                                 System.Console.Write("Cancel");
                                 System.Console.ResetColor();
@@ -255,7 +248,7 @@ namespace PatzminiHD.CSLib.Input
                                 System.Console.Write("Cancel");
                                 System.Console.SetCursorPosition(xLeft - 14, y);
                                 System.Console.Write("Yes");
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 10, y);
                                 System.Console.Write("No");
                                 System.Console.ResetColor();
@@ -265,7 +258,7 @@ namespace PatzminiHD.CSLib.Input
                                 System.Console.Write("Cancel");
                                 System.Console.SetCursorPosition(xLeft - 10, y);
                                 System.Console.Write("No");
-                                SwapForegroundBackgroundColor();
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
                                 System.Console.SetCursorPosition(xLeft - 14, y);
                                 System.Console.Write("Yes");
                                 System.Console.ResetColor();
@@ -535,6 +528,204 @@ namespace PatzminiHD.CSLib.Input
                 return selectedResponse;
             }
         }
-        
+
+        /// <summary>
+        /// Contains Methods for showing a drop down menu
+        /// </summary>
+        public static class DropDownMenu
+        {
+            private static void WriteSelections((int left, int top) startingPosition, List<string> selections, int readerOffset, int xEnd)
+            {
+                if (System.Console.WindowHeight - startingPosition.top > selections.Count())
+                {
+                    //No scrolling needed
+                    Visuals.Console.Color.SwapForegroundBackgroundColor();
+
+                    for (int i = 0; i < selections.Count(); i++)
+                    {
+                        if (readerOffset != -1 && (i == readerOffset || i == readerOffset + 1))
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                        System.Console.SetCursorPosition(startingPosition.left, startingPosition.top + i + 1);
+                        System.Console.Write(selections[i]);
+
+                        if (i != readerOffset)
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                        for (int j = System.Console.CursorLeft; j < xEnd; j++)
+                        {
+                            System.Console.Write(' ');
+                        }
+                        if (i != readerOffset)
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                    }
+                }
+                else if (startingPosition.top > selections.Count())
+                {
+                    //Drop up
+                    Visuals.Console.Color.SwapForegroundBackgroundColor();
+
+                    for (int i = 0; i < selections.Count(); i++)
+                    {
+                        if (readerOffset != -1 && (i == readerOffset || i == readerOffset + 1))
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                        System.Console.SetCursorPosition(startingPosition.left, startingPosition.top - selections.Count() + i);
+                        System.Console.Write(selections[i]);
+
+                        if (i != readerOffset)
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                        for (int j = System.Console.CursorLeft; j < xEnd; j++)
+                        {
+                            System.Console.Write(' ');
+                        }
+                        if (i != readerOffset)
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                    }
+                }
+                else if (System.Console.WindowHeight - startingPosition.top > startingPosition.top)
+                {
+                    //Drop down with scrolling
+                    Visuals.Console.Color.SwapForegroundBackgroundColor();
+
+                    for (int i = 0; i < System.Console.WindowHeight - startingPosition.top - 1; i++)
+                    {
+                        int lastPos = i + readerOffset - (System.Console.WindowHeight - startingPosition.top - 2);
+                        System.Console.SetCursorPosition(startingPosition.left, startingPosition.top + i + 1);
+                        if(readerOffset > System.Console.WindowHeight - startingPosition.top - 2)
+                        {
+                            if (readerOffset != -1 && (lastPos == readerOffset || lastPos == readerOffset + 1))
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
+                            System.Console.Write(selections[lastPos]);
+                        }
+                        else
+                        {
+                            if (readerOffset != -1 && (i == readerOffset || i == readerOffset + 1))
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
+                            System.Console.Write(selections[i]);
+                        }
+
+                        if (i != readerOffset && (readerOffset <= System.Console.WindowHeight - startingPosition.top - 2 || lastPos != readerOffset))
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                        for (int j = System.Console.CursorLeft; j < xEnd; j++)
+                        {
+                            System.Console.Write(' ');
+                        }
+                        if (i != readerOffset && (readerOffset <= System.Console.WindowHeight - startingPosition.top - 2 || lastPos != readerOffset))
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                    }
+                }
+                else
+                {
+                    //Drop up with scrolling
+                    Visuals.Console.Color.SwapForegroundBackgroundColor();
+
+                    for (int i = 0; i < startingPosition.top; i++)
+                    {
+                        System.Console.SetCursorPosition(startingPosition.left, i);
+
+                        int lastPos = i + readerOffset - (selections.Count() - startingPosition.top);
+
+                        if (readerOffset < selections.Count() - startingPosition.top + 1)
+                        {
+                            if (readerOffset != -1 && (i == readerOffset || i == readerOffset + 1))
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
+                            System.Console.Write(selections[i]);
+                        }
+                        else
+                        {
+                            if (readerOffset != -1 && (lastPos == readerOffset || lastPos == readerOffset + 1))
+                                Visuals.Console.Color.SwapForegroundBackgroundColor();
+                            System.Console.Write(selections[lastPos]);
+                        }
+
+
+                        if (i != readerOffset && (i != startingPosition.top - 1 || readerOffset < selections.Count() - startingPosition.top + 1))
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                        for (int j = System.Console.CursorLeft; j < xEnd; j++)
+                        {
+                            System.Console.Write(' ');
+                        }
+                        if (i != readerOffset && i != startingPosition.top - 1)
+                            Visuals.Console.Color.SwapForegroundBackgroundColor();
+                    }
+                }
+
+                System.Console.ResetColor();
+            }
+            /// <summary>
+            /// Show a DropDownMenu
+            /// </summary>
+            /// <param name="options"></param>
+            /// <param name="defaultSelection"></param>
+            /// <returns></returns>
+            public static int Show(List<string> options, int defaultSelection = -1)
+            {
+                var startingPosition = System.Console.GetCursorPosition();
+
+                for (int i = 0; i < options.Count(); i++)
+                {
+                    if (options[i].Length > System.Console.WindowWidth - startingPosition.Left)
+                        options[i] = options[i].Substring(0, System.Console.WindowWidth - startingPosition.Left - 4) + "...";
+                }
+
+                int xEnd = 0;
+                foreach (string selection in options)
+                {
+                    if (selection.Length > xEnd)
+                        xEnd = selection.Length;
+                }
+                xEnd += startingPosition.Left;
+
+                bool flag = true;
+                int readerOffset = defaultSelection;
+
+                while(flag)
+                {
+                    System.Console.SetCursorPosition(startingPosition.Left, startingPosition.Top);
+                    if(readerOffset >= 0 && readerOffset < options.Count())
+                        System.Console.Write(options[readerOffset]);
+                    else
+                        System.Console.Write("<Select>");
+
+                    for (int i = System.Console.CursorLeft; i < xEnd; i++)
+                    {
+                        System.Console.Write(' ');
+                    }
+
+                    WriteSelections(startingPosition, options, readerOffset, xEnd);
+
+                    switch(System.Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.Enter:
+                            flag = false;
+                            break;
+                        case ConsoleKey.Escape:
+                            readerOffset = defaultSelection;
+                            flag = false;
+                            break;
+                        case ConsoleKey.UpArrow:
+                        case ConsoleKey.K:
+                            if (readerOffset > 0)
+                                readerOffset--;
+                            else if(readerOffset <= 0)
+                                readerOffset = options.Count - 1;
+                            break;
+                        case ConsoleKey.DownArrow:
+                        case ConsoleKey.J:
+                            if(readerOffset < options.Count - 1)
+                                readerOffset++;
+                            else
+                                readerOffset = 0;
+                            break;
+                        case ConsoleKey.PageUp:
+                            readerOffset = 0;
+                            break;
+                        case ConsoleKey.PageDown:
+                            readerOffset = options.Count - 1;
+                            break;
+                    }
+                }
+
+                return readerOffset;
+            }
+        }
     }
 }
---
+++ PatzminiHD.CSLib/Visuals/Console/Color.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatzminiHD.CSLib.Visuals.Console
+{
+    /// <summary>
+    /// Contains Methods for manipulating Console Colors
+    /// </summary>
+    public class Color
+    {
+        /// <summary>
+        /// Swap Foreground and Background Color
+        /// </summary>
+        public static void SwapForegroundBackgroundColor()
+        {
+            var tmp = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = System.Console.BackgroundColor;
+            System.Console.BackgroundColor = tmp;
+        }
+    }
+}
