25000ef59bbe32b8faad909af3356eab2ae3a06a
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2024-05-18 20:12:44 +0200

    Add TryGet.UInt

--- PatzminiHD.CSLib/Input/Console/TryGet.cs
+++ PatzminiHD.CSLib/Input/Console/TryGet.cs
@@ -12,6 +12,28 @@ namespace PatzminiHD.CSLib.Input.Console
     public class TryGet
     {
         /// <summary>
+        /// Get an unsigned integer from the user
+        /// </summary>
+        /// <param name="value">The value the user entered. 0 if the user cancelled the input</param>
+        /// <param name="message">The message to display to the user</param>
+        /// <param name="emptyToCancel">True if the user can enter nothing to cancel the input</param>
+        /// <returns>True if the input was valid<br/>False if the input was cancelled</returns>
+        public static bool UInt(out uint value, string message, bool emptyToCancel = true)
+        {
+            value = 0;
+            System.Console.Write(message);
+            var userInput = System.Console.ReadLine();
+            while (!uint.TryParse(userInput, out value))
+            {
+                if ((userInput == null || userInput == "") && emptyToCancel)
+                    break;
+                System.Console.Write("Invalid input. " + message);
+                userInput = System.Console.ReadLine();
+            }
+
+            return userInput == null || userInput == "" ? false : true;
+        }
+        /// <summary>
         /// Get an integer from the user
         /// </summary>
         /// <param name="value">The value the user entered. 0 if the user cancelled the input</param>
