f9f2f46f17d10c4ef95b41e71bb231d2ab032ece
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2024-12-01 21:04:57 +0100

    Add Tests for all ExtensionMethods

---
+++ PatzminiHD.CSLibTest/ExtensionMethodsTests/StringTests.cs
@@ -0,0 +1,20 @@
+﻿using PatzminiHD.CSLib.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatzminiHD.CSLibTest.ExtensionMethodsTests
+{
+    [TestClass]
+    public class StringTests
+    {
+        [TestMethod]
+        public void TestCount()
+        {
+            Assert.AreEqual(3, "This is a test string".Count('i'));
+            Assert.AreEqual(0, "This is a test string".Count('z'));
+        }
+    }
+}
---
+++ PatzminiHD.CSLibTest/ExtensionMethodsTests/TimeSpanExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using PatzminiHD.CSLib.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatzminiHD.CSLibTest.ExtensionMethodsTests
+{
+    [TestClass]
+    public class TimeSpanExtensionTests
+    {
+        [TestMethod]
+        public void TestToString()
+        {
+            TimeSpan timeSpan = new TimeSpan(10, 5, 3, 6);
+
+            Assert.AreEqual("10:5:03:06", TimeSpanExtension.ToString(timeSpan, "DD:HH:MM:SS"));
+        }
+
+    }
+}
