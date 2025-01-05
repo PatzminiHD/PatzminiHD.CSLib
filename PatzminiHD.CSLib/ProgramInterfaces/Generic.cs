b8957d4fad01003bb863467995d307b9a019be8f
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2025-01-05 15:41:14 +0100

    Add ffmpeg ProgramInterface

---
+++ PatzminiHD.CSLib/ProgramInterfaces/Generic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatzminiHD.CSLib.ProgramInterfaces
+{
+    /// <summary>
+    /// Contains Methods to start and interact with processes
+    /// </summary>
+    public class Generic
+    {
+        /// <summary>
+        /// Start a process
+        /// </summary>
+        /// <param name="fileName">The name of the process</param>
+        /// <param name="arguments">The arguments that should be given to the process</param>
+        /// <param name="useShellExecute">Wether to use shell execute</param>
+        /// <param name="redirectStandardOutput">Wether to redirect the standard output</param>
+        public static void StartProcess(string fileName, string arguments, bool useShellExecute = false, bool redirectStandardOutput = true)
+        {
+            if(redirectStandardOutput && useShellExecute)
+                throw new ArgumentException("useShellExecute has to be false to redirect the standard output");
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = useShellExecute,
+                RedirectStandardOutput = redirectStandardOutput,
+                FileName = fileName,
+                Arguments = arguments,
+            };
+
+            Process proc = new Process();
+            proc.StartInfo = startInfo;
+            proc.Start();
+            proc.WaitForExit();
+            proc.Dispose();
+        }
+    }
+}
---
+++ PatzminiHD.CSLib/ProgramInterfaces/ffmpeg.cs
@@ -0,0 +1,49 @@
+﻿
+
+namespace PatzminiHD.CSLib.ProgramInterfaces
+{
+    /// <summary>
+    /// Contains Methods for interacting with FFmpeg
+    /// </summary>
+    public class FFmpeg
+    {
+        //ffmpeg -i <input_file> -c:v libsvtav1 -crf <quality> -cpu-used <num> -row-mt <num> <output_file>
+        /// <summary>
+        /// Reencode a video file to AV1 using ffmpeg/libsvtav1
+        /// </summary>
+        /// <param name="filename">The file you want to reencode</param>
+        /// <param name="newFileName">The output filepath</param>
+        /// <param name="quality">The quality ("crf" parameter) of the encoding:<br/>
+        /// The CRF value can be from 0–63. Lower values mean better quality and greater file size. 0 means lossless. A CRF value of 23 yields a quality level corresponding to CRF 19 for x264, which would be considered visually lossless.</param>
+        /// <param name="cpu_used">The cpu-used parameter of ffmpeg<br/>
+        /// -cpu-used sets how efficient the compression will be. Default is 1. Lower values mean slower encoding with better quality, and vice-versa. Valid values are from 0 to 8 inclusive</param>
+        /// <param name="row_mt">True to enable multi-threading<br/>
+        /// -row-mt 1 enables row-based multi-threading which maximizes CPU usage. To enable fast decoding performance, also add tiles (i.e. -tiles 4x1 or -tiles 2x2 for 4 tiles). Enabling row-mt is only faster when the CPU has more threads than the number of encoded tiles</param>
+        /// <returns></returns>
+        public static Exception? ReencodeVideo(string filename, string newFileName, uint quality, uint cpu_used = 1, bool row_mt = false)
+        {
+            if (Environment.Get.OS != Environment.Get.OperatingSystem.Linux)
+                return new NotSupportedException("FFmpeg is currently only supported on linux");
+
+            if (quality > 63) //Value can only range from 0 to 63 (inclusive)
+                return new ArgumentException("quality can only range from 0 to 63");
+
+            if(cpu_used > 8) //Value can only range from 0 to 8 (inclusive)
+                return new ArgumentException("cpu_used can only range from 0 to 8");
+
+            if(!File.Exists(filename))
+                return new ArgumentException("File to encode does not exist");
+
+            if (File.Exists(newFileName))
+                return new ArgumentException("Output file already exists");
+
+            string arguments = $"-i {filename} -c:v libsvtav1 -crf {quality} -cpu-used {cpu_used} ";
+            if (row_mt)
+                arguments += $"-row-mt 1 ";
+            arguments += $"{newFileName}";
+
+            Generic.StartProcess("ffmpeg", arguments);
+            return null;
+        }
+    }
+}
