723e38c3f32ddc9d87cfc8e4e6e51e071186a724
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2025-02-11 11:19:32 +0100

    Finish Logging class (Fixes #19)

--- PatzminiHD.CSLib/Output/Logging.cs
+++ PatzminiHD.CSLib/Output/Logging.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace PatzminiHD.CSLib.Output
 {
     /// <summary>
-    /// Contains Methods for logging purposes
+    /// Contains Methods for logging purposes<br/>
+    /// Defaults: Log everything to the console, nothing to a file
     /// </summary>
     public static class Logging
     {
-        private static string? _logFilePath;
+        private static string? _logFilePath = null;
         /// <summary> Log entries of this and higher levels are logged to the logfile<br/>
         ///           A value of null means that nothing is logged to the logfile</summary>
         public static LoggingLevel? FileLogLevel { get; set; } = null;
         /// <summary> Log entries of this and higher levels are logged to the console<br/>
         ///           A value of null means that nothing is logged to the console</summary>
-        public static LoggingLevel? ConsoleLogLevel { get; set; } = null;
+        public static LoggingLevel? ConsoleLogLevel { get; set; } = LoggingLevel.Info;
         /// <summary> The path of the logfile.
         ///           If set to null, nothing will be logged, regardless of <see cref="FileLogLevel"/> </summary>
         public static string? LogFilePath
@@ -28,10 +24,11 @@ namespace PatzminiHD.CSLib.Output
             {
                 try
                 {
+                    var fileName = DateTime.UtcNow.ToString("yyMMdd-HHmmss") + '_' + value;
                     if (value != null && !File.Exists(value))
                         File.Create(value);
 
-                    _logFilePath = value;
+                    _logFilePath = fileName;
                 }
                 catch(Exception e)
                 {
@@ -52,14 +49,114 @@ namespace PatzminiHD.CSLib.Output
             ERROR = 2,
         }
 
-        public static void Log(string message, LoggingLevel logLevel)
+        /// <summary>
+        /// Initialise the logging.<br/>
+        /// It is recommended that you also call the CullLogs Methods<br/>
+        /// (<see cref="CullLogs(int)"/> or <see cref="CullLogs(DateTime)"/>)
+        /// </summary>
+        /// <param name="consoleLogLevel">The lowest level of log that will be logged to the console</param>
+        /// <param name="fileLogLevel">The lowest level of log that will be logged to the log file<br/>
+        ///                            (Is only logged when <see cref="LogFilePath"/> is also valid)</param>
+        /// <param name="logFilePath">The path of the log file<br/>
+        ///                           (Logs are only written to the file if <see cref="FileLogLevel"/> is also set)</param>
+        public static void Init(LoggingLevel? consoleLogLevel, LoggingLevel? fileLogLevel, string? logFilePath)
         {
+            ConsoleLogLevel = consoleLogLevel;
+            FileLogLevel = fileLogLevel;
+            LogFilePath = logFilePath;
+        }
+
+
+        /// <summary>
+        /// Cull all logs that are older than <paramref name="oldestLogToKeep"/>
+        /// </summary>
+        /// <param name="oldestLogToKeep">The oldest log to keep</param>
+        public static void CullLogs(DateTime oldestLogToKeep)
+        {
+            if (LogFilePath == null)
+                return;
+
+            foreach (var logFile in GetExistingLogFiles())
+            {
+                try
+                {
+                    var fileTime = DateTime.ParseExact(Path.GetFileName(logFile).Substring(0, 13), "yyMMdd-HHmmss", null);
+                    if (fileTime < oldestLogToKeep)
+                        File.Delete(logFile);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Cull all logs, keeping only the latest <paramref name="numOfLogsToKeep"/>
+        /// </summary>
+        /// <param name="numOfLogsToKeep">How many logs to keep</param>
+        public static void CullLogs(int numOfLogsToKeep)
+        {
+            foreach (var logFile in GetExistingLogFiles().SkipLast(numOfLogsToKeep))
+            {
+                File.Delete(logFile);
+            }
+        }
+
+
+        /// <summary>
+        /// Log a message with <see cref="LoggingLevel.Info"/>
+        /// </summary>
+        /// <param name="message">The message to log</param
+        /// <param name="subsystem">Optional: The name of a subsystem where the log originated from,
+        ///                         for easier tracing</param>
+        public static void LogInfo(string message, string subsystem = "")
+        {
+            Log(message, LoggingLevel.Info, subsystem);
+        }
+
+        /// <summary>
+        /// Log a message with <see cref="LoggingLevel.Warning"/>
+        /// </summary>
+        /// <param name="message">The message to log</param
+        /// <param name="subsystem">Optional: The name of a subsystem where the log originated from,
+        ///                         for easier tracing</param>
+        public static void LogWarning(string message, string subsystem = "")
+        {
+            Log(message, LoggingLevel.Warning, subsystem);
+        }
+
+        /// <summary>
+        /// Log a message with <see cref="LoggingLevel.ERROR"/>
+        /// </summary>
+        /// <param name="message">The message to log</param
+        /// <param name="subsystem">Optional: The name of a subsystem where the log originated from,
+        ///                         for easier tracing</param>
+        public static void LogError(string message, string subsystem = "")
+        {
+            Log(message, LoggingLevel.ERROR, subsystem);
+        }
+
+        /// <summary>
+        /// Add a message to the log, with a specified loglevel
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="logLevel">The loglevel</param>
+        /// <param name="subsystem">Optional: The name of a subsystem where the log originated from,
+        ///                         for easier tracing</param>
+        public static void Log(string message, LoggingLevel logLevel, string subsystem = "")
+        {
+            //Improve performence when no logging is needed
+            if (ConsoleLogLevel == null && FileLogLevel == null)
+                return;
+
             var callerMethodName = (new System.Diagnostics.StackTrace()).GetFrame(1)?.GetMethod()?.Name;
             var dateTimeString = DateTime.UtcNow.ToString("yyyy.MM.dd HH:mm:ss");
 
-            string logMessage = $"{dateTimeString} [{Enum.GetName(typeof(LoggingLevel), logLevel)}] ({callerMethodName}) {message}";
+            string logMessage = $"{dateTimeString} [{Enum.GetName(typeof(LoggingLevel), logLevel)}] ({subsystem}:{callerMethodName}) {message}";
 
-
+            LogToConsole(logMessage, logLevel);
+            LogToFile(logMessage, logLevel);
         }
 
         private static void LogToFile(string message, LoggingLevel loggingLevel)
@@ -88,6 +185,21 @@ namespace PatzminiHD.CSLib.Output
             }
             System.Console.WriteLine(message);
             System.Console.ResetColor();
+        }
+
+        private static List<string> GetExistingLogFiles()
+        {
+            if(LogFilePath == null)
+                return new();
+
+            var logDir = new DirectoryInfo(LogFilePath).FullName;
+
+            //Match strings like "250211-102415" where each digit can be exchanged for any other digit
+            Regex regex = new("^[0-9]{6}-[0-9]{6}_");
+
+            var logFiles = Directory.GetFiles(logDir).Where(f => regex.IsMatch(Path.GetFileName(f))).ToList();
+            logFiles.Sort();
+            return logFiles;
         }
     }
 }
